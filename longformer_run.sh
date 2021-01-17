35c9d19e554f76144ceef18eff9b90a78ef47d5f
guangtao.wang <guangtao.wang@jd.com>
2021-01-17 15:23:54 -0800

    Initial commit

--- longformer_run.sh
+++ longformer_run.sh
@@ -72,13 +72,14 @@ preprocess() {
 #        # switch to Longformer for final leaderboard
 #        python longformerscripts/3_longformer_paragraph_ranking.py --data_dir $OUTPUT_PROCESSED --eval_ckpt $DATA_ROOT/models/finetuned/PS/longformer_pytorchlighting_model.ckpt --raw_data $INPUT_FILE --input_data $OUTPUT_PROCESSED/para_ir_combined.json
 
-        echo "4. MultiHop Paragraph Selection"
-        # Input: $INPUT_FILE, doc_link_ner.json,  ner.json, long_para_ranking.json
-        # Output: long_multihop_para.json
-        python scripts/4_multihop_ps.py $INPUT_FILE $OUTPUT_PROCESSED/doc_link_ner.json $OUTPUT_PROCESSED/ner.json $OUTPUT_PROCESSED/long_para_ranking.json $OUTPUT_PROCESSED/long_multihop_para.json
+#        echo "4. MultiHop Paragraph Selection"
+#        # Input: $INPUT_FILE, doc_link_ner.json,  ner.json, long_para_ranking.json
+#        # Output: long_multihop_para.json
+#        python scripts/4_multihop_ps.py $INPUT_FILE $OUTPUT_PROCESSED/doc_link_ner.json $OUTPUT_PROCESSED/ner.json $OUTPUT_PROCESSED/long_para_ranking.json $OUTPUT_PROCESSED/long_multihop_para.json
 
-#        echo "5. Dump features"
+        echo "5. Dump features"
 #        python longformerscripts/5_longformer_dump_features.py --para_path $OUTPUT_PROCESSED/long_multihop_para.json --raw_data $INPUT_FILE --model_name_or_path $LONG_FORMER_ROOT/longformer-large-4096 --do_lower_case --ner_path $OUTPUT_PROCESSED/ner.json --model_type longformer --tokenizer_name longformer-large-4096 --output_dir $OUTPUT_FEAT --doc_link_ner $OUTPUT_PROCESSED/doc_link_ner.json
+        python scripts/5_longformer_dump_features.py --para_path $OUTPUT_PROCESSED/long_multihop_para.json --raw_data $INPUT_FILE --model_name_or_path roberta-large --ner_path $OUTPUT_PROCESSED/ner.json --model_type roberta --tokenizer_name roberta-large --output_dir $OUTPUT_FEAT --doc_link_ner $OUTPUT_PROCESSED/doc_link_ner.json
 #        python scripts/5_dump_features.py --para_path $OUTPUT_PROCESSED/long_multihop_para.json --raw_data $INPUT_FILE --model_name_or_path roberta-large --do_lower_case --ner_path $OUTPUT_PROCESSED/ner.json --model_type roberta --tokenizer_name roberta-large --output_dir $OUTPUT_FEAT --doc_link_ner $OUTPUT_PROCESSED/doc_link_ner.json
 #        python scripts/5_dump_features.py --para_path $OUTPUT_PROCESSED/long_multihop_para.json --raw_data $INPUT_FILE --model_name_or_path albert-xxlarge-v2 --do_lower_case --ner_path $OUTPUT_PROCESSED/ner.json --model_type albert --tokenizer_name albert-xxlarge-v2 --output_dir $OUTPUT_FEAT --doc_link_ner $OUTPUT_PROCESSED/doc_link_ner.json
 
