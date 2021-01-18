ded15ccfccac449fd82496fe8ce5744112c8d024
guangtao.wang <guangtao.wang@jd.com>
2021-01-17 18:17:59 -0800

    Initial commit

--- model_envs.py
+++ model_envs.py
@@ -4,14 +4,12 @@ from hgntransformers import (BertConfig, BertTokenizer, BertModel,
                              AlbertConfig, AlbertTokenizer, AlbertModel)
 from hgntransformers import (BertModel, XLNetModel, RobertaModel)
 from transformers import LongformerModel, LongformerTokenizer, LongformerConfig
-from transformers import configuration_longformer
 
 ############################################################
 # Model Related Global Varialbes
 ############################################################
 
 ALL_MODELS = sum((tuple(conf.pretrained_config_archive_map.keys()) for conf in (BertConfig, RobertaConfig, AlbertConfig)), ())
-ALL_LONG_MODELS = sum((tuple(configuration_longformer.LONGFORMER_PRETRAINED_CONFIG_ARCHIVE_MAP.keys())), ())
 
 MODEL_CLASSES = {
     'bert': (BertConfig, BertModel, BertTokenizer),
