c40f469aef0b86ede7f93568815957359f5544b6
guangtao.wang <guangtao.wang@jd.com>
2021-01-21 14:09:11 -0800

    longformer

--- errorAnalysis/predictionAnalysis.py
+++ errorAnalysis/predictionAnalysis.py
@@ -185,7 +185,9 @@ def error_analysis(raw_data, examples, features, predictions, tokenizer, use_ent
 
 
     conf_matrix = confusion_matrix(yes_no_span_true, yes_no_span_predictions, labels=["yes", "no", "span"])
+    conf_ans_sent_matrix = confusion_matrix(pred_ans_type_list, pred_sent_type_list, labels=['em', 'sub_set', 'super_set', 'others'])
     print('Ans type conf matrix {}'.format(conf_matrix))
+    print('Type conf matrix {}'.format(conf_ans_sent_matrix))
     print("Ans prediction type: {}".format(prediction_ans_type_counter))
     print("Sent prediction type: {}".format(prediction_sent_type_counter))
 
