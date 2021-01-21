3baeb40beb8acda197e33c06b15be322780eba5e
guangtao.wang <guangtao.wang@jd.com>
2021-01-21 09:44:52 -0800

    longformer

--- longformerscripts/6_longformer_test_features.py
+++ longformerscripts/6_longformer_test_features.py
@@ -16,62 +16,77 @@ from envs import DATASET_FOLDER, OUTPUT_FOLDER
 from eval.hotpot_evaluate_v1 import eval as hotpot_eval
 from eval.hotpot_evaluate_v1 import normalize_answer
 
-# features
-# self.qas_id = qas_id
-#         self.doc_tokens = doc_tokens
-#         self.doc_input_ids = doc_input_ids
-#         self.doc_input_mask = doc_input_mask
-#         self.doc_segment_ids = doc_segment_ids
-#
-#         self.query_tokens = query_tokens
-#         self.query_input_ids = query_input_ids
-#         self.query_input_mask = query_input_mask
-#         self.query_segment_ids = query_segment_ids
-#
-#         self.para_spans = para_spans
-#         self.sent_spans = sent_spans
-#         self.entity_spans = entity_spans
-#         self.q_entity_cnt = q_entity_cnt
-#         self.sup_fact_ids = sup_fact_ids
-#         self.sup_para_ids = sup_para_ids
-#         self.ans_type = ans_type
-#
-#         self.edges = edges
-#         self.token_to_orig_map = token_to_orig_map
-#         self.orig_answer_text = orig_answer_text
-#         self.answer_in_entity_ids = answer_in_entity_ids
-#         self.answer_candidates_ids = answer_candidates_ids
-#
-#         self.start_position = start_position
-#         self.end_position = end_position
+def exmple_infor_collection(example: Example):
+    # Example
+    # self.qas_id = qas_id
+    #         self.qas_type = qas_type
+    #         self.question_tokens = question_tokens
+    #         self.doc_tokens = doc_tokens
+    #         self.question_text = question_text
+    #         self.sent_num = sent_num
+    #         self.sent_names = sent_names
+    #         self.para_names = para_names
+    #         self.sup_fact_id = sup_fact_id
+    #         self.sup_para_id = sup_para_id
+    #         self.ques_entities_text = ques_entities_text
+    #         self.ctx_entities_text = ctx_entities_text
+    #         self.para_start_end_position = para_start_end_position
+    #         self.sent_start_end_position = sent_start_end_position
+    #         self.ques_entity_start_end_position = ques_entity_start_end_position
+    #         self.ctx_entity_start_end_position = ctx_entity_start_end_position
+    #         self.question_word_to_char_idx = question_word_to_char_idx
+    #         self.ctx_text = ctx_text
+    #         self.ctx_word_to_char_idx = ctx_word_to_char_idx
+    #         self.edges = edges
+    #         self.orig_answer_text = orig_answer_text
+    #         self.answer_in_ques_entity_ids = answer_in_ques_entity_ids
+    #         self.answer_in_ctx_entity_ids = answer_in_ctx_entity_ids
+    #         self.answer_candidates_in_ctx_entity_ids= answer_candidates_in_ctx_entity_ids
+    #         self.start_position = start_position
+    #         self.end_position = end_position
+    doc_tokens = example.doc_tokens
+    query_tokens = example.question_tokens
+    sent_num = example.sent_num
+    sent_start_end_position = example.sent_start_end_position
+    ent_start_end_position = example.ctx_entity_start_end_position
+    print(sent_num, len(sent_start_end_position))
 
-# Example
-# self.qas_id = qas_id
-#         self.qas_type = qas_type
-#         self.question_tokens = question_tokens
-#         self.doc_tokens = doc_tokens
-#         self.question_text = question_text
-#         self.sent_num = sent_num
-#         self.sent_names = sent_names
-#         self.para_names = para_names
-#         self.sup_fact_id = sup_fact_id
-#         self.sup_para_id = sup_para_id
-#         self.ques_entities_text = ques_entities_text
-#         self.ctx_entities_text = ctx_entities_text
-#         self.para_start_end_position = para_start_end_position
-#         self.sent_start_end_position = sent_start_end_position
-#         self.ques_entity_start_end_position = ques_entity_start_end_position
-#         self.ctx_entity_start_end_position = ctx_entity_start_end_position
-#         self.question_word_to_char_idx = question_word_to_char_idx
-#         self.ctx_text = ctx_text
-#         self.ctx_word_to_char_idx = ctx_word_to_char_idx
-#         self.edges = edges
-#         self.orig_answer_text = orig_answer_text
-#         self.answer_in_ques_entity_ids = answer_in_ques_entity_ids
-#         self.answer_in_ctx_entity_ids = answer_in_ctx_entity_ids
-#         self.answer_candidates_in_ctx_entity_ids= answer_candidates_in_ctx_entity_ids
-#         self.start_position = start_position
-#         self.end_position = end_position
+    return
+
+def feature_infor_collection(feature: InputFeatures):
+    # features
+    # self.qas_id = qas_id
+    #         self.doc_tokens = doc_tokens
+    #         self.doc_input_ids = doc_input_ids
+    #         self.doc_input_mask = doc_input_mask
+    #         self.doc_segment_ids = doc_segment_ids
+    #
+    #         self.query_tokens = query_tokens
+    #         self.query_input_ids = query_input_ids
+    #         self.query_input_mask = query_input_mask
+    #         self.query_segment_ids = query_segment_ids
+    #
+    #         self.para_spans = para_spans
+    #         self.sent_spans = sent_spans
+    #         self.entity_spans = entity_spans
+    #         self.q_entity_cnt = q_entity_cnt
+    #         self.sup_fact_ids = sup_fact_ids
+    #         self.sup_para_ids = sup_para_ids
+    #         self.ans_type = ans_type
+    #
+    #         self.edges = edges
+    #         self.token_to_orig_map = token_to_orig_map
+    #         self.orig_answer_text = orig_answer_text
+    #         self.answer_in_entity_ids = answer_in_entity_ids
+    #         self.answer_candidates_ids = answer_candidates_ids
+    #
+    #         self.start_position = start_position
+    #         self.end_position = end_position
+    doc_tokens = feature.doc_tokens
+    sent_spans = feature.sent_spans
+    ent_spans = feature.entity_spans
+
+    return
 
 def predict(raw_data, examples, features, pred_file, tokenizer, use_ent_ans=False):
     answer_dict = dict()
@@ -116,18 +131,10 @@ def predict(raw_data, examples, features, pred_file, tokenizer, use_ent_ans=Fals
         f_sent_span = feature.sent_spans
         f_ent_span = feature.entity_spans
         print('entity number', len(e_ent_span) + len(e_q_ent_span), len(f_ent_span))
-        # if len(e_sent_span) < len(f_sent_span):
-        #     print(len(e_sent_span), len(f_sent_span))
 
-        # print(len(q_tokens), len(e_doc_tokens), len(f_doc_tokens), len(f_doc_input_ids))
-        # if len(e_doc_tokens) > len(f_doc_tokens):
-        #     print(len(q_tokens), len(e_doc_tokens), len(f_doc_tokens), len(f_doc_input_ids))
-        #     # print(e_doc_tokens)
-        #     # print('-'*75)
-        #     # print(f_doc_tokens)
-        #     print('+' * 75)
         deep_token_num_list.append(len(f_doc_tokens))
         print('token number spacy tokenizer: {}, deep tokenizer {}'.format(len(e_doc_tokens) + len(e_q_tokens), len(f_doc_tokens)))
+        deep_token_decoded_str = tokenizer.convert_tokens_to_string
 
     print('Max token num = {}'.format(max_token_num))
     print('Max deep token num = {}'.format(max_deep_token_num))
