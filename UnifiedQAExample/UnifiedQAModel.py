399e2f8026fcf8eb3e7df9dce5544c35c8e9613a
guangtao.wang <guangtao.wang@jd.com>
2021-01-26 15:35:13 -0800

    longformer

--- UnifiedQAExample/UnifiedQAModel.py
+++ UnifiedQAExample/UnifiedQAModel.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer, T5ForConditionalGeneration
 
 def unifiedqa_model_loader(model_name: str):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = T5ForConditionalGeneration.from_pretrained(model_name)
+    model = T5ForConditionalGeneration.from_pretrained(model_name, from_tf=True)
     return model, tokenizer
 
 def unified_qa_prediction(model, tokenizer, question: str, context: str, **generator_args):
