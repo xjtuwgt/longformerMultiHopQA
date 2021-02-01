2de3d55fcee399dd5e2bb81c8b6f1a2d1c2ad91e
guangtao.wang <guangtao.wang@jd.com>
2021-02-01 11:18:00 -0800

    longformer

--- jd_mhqa/lossUtils.py
+++ jd_mhqa/lossUtils.py
@@ -1,4 +1,4 @@
-###Adaptive threhold learning pair-wised loss
+### Adaptive threhold learning pair-wised loss
 """
 References: Document-Level Relation Extraction with Adaptive Thresholding and Localized Context Pooling
 References: Focal Loss for Dense Object Detection
