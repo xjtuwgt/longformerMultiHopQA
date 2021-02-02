d3b5d3c9bf9d7ba0d16e9da636bb3a0b386b4194
guangtao.wang <guangtao.wang@jd.com>
2021-02-01 18:01:48 -0800

    longformer

--- jd_mhqa/lossUtils.py
+++ jd_mhqa/lossUtils.py
@@ -12,8 +12,9 @@ class ATLoss(nn.Module):
     """
     Document-Level Relation Extraction with Adaptive Thresholding and Localized Context Pooling
     """
-    def __init__(self):
+    def __init__(self, reduction: str = 'mean'):
         super().__init__()
+        self.reduction = reduction
 
     def forward(self, logits, labels):
         # TH label
@@ -32,7 +33,62 @@ class ATLoss(nn.Module):
         # Rank positive classes to TH
         logit1 = logits - (1 - p_mask) * 1e30
         loss1 = -(F.log_softmax(logit1, dim=-1) * labels).sum(1)
+        ##################################################################
+        if self.reduction == 'mean':
+            labels_count = labels.sum(1)
+            loss1 = loss1/labels_count
+        ##################################################################
+        # Rank TH to negative classes
+        logit2 = logits - (1 - n_mask) * 1e30
+        loss2 = -(F.log_softmax(logit2, dim=-1) * th_label).sum(1)
 
+        # Sum two parts
+        loss = loss1 + loss2
+        loss = loss.mean()
+        return loss
+
+    def get_label(self, logits, num_labels=-1):
+        th_logit = logits[:, 0].unsqueeze(1)
+        output = torch.zeros_like(logits).to(logits)
+        mask = (logits > th_logit)
+        if num_labels > 0:
+            top_v, _ = torch.topk(logits, num_labels, dim=1)
+            top_v = top_v[:, -1]
+            mask = (logits >= top_v.unsqueeze(1)) & mask
+        output[mask] = 1.0
+        output[:, 0] = (output.sum(1) == 0.).to(logits)
+        return output
+
+class ATMLoss(nn.Module):
+    def __init__(self, reduction: str = 'mean'):
+        super().__init__()
+        self.reduction = reduction
+
+    def forward(self, logits, labels, mask = None):
+        # TH label
+        """
+        :param logits: batch_size * (number of labels + 1) the first column is for threshold label
+        :param labels: batch_size * number of labels (0, 1) matrix, the first column corresponding to the threshold labels
+        :return: loss_1 (positive loss), loss_2 (negative loss)
+        """
+        th_label = torch.zeros_like(labels, dtype=torch.float).to(labels)
+        th_label[:, 0] = 1.0  ## threshold based true label information
+        labels[:, 0] = 0.0  ## true positive labels
+
+        p_mask = labels + th_label
+        n_mask = 1 - labels
+        ##################################################################
+        if mask is not None:
+            logits = logits.masked_fill(mask == 0, -1e30)
+        ##################################################################
+        # Rank positive classes to TH
+        logit1 = logits - (1 - p_mask) * 1e30
+        loss1 = -(F.log_softmax(logit1, dim=-1) * labels).sum(1)
+        ##################################################################
+        if self.reduction == 'mean':
+            labels_count = labels.sum(1)
+            loss1 = loss1 / labels_count
+        ##################################################################
         # Rank TH to negative classes
         logit2 = logits - (1 - n_mask) * 1e30
         loss2 = -(F.log_softmax(logit2, dim=-1) * th_label).sum(1)
