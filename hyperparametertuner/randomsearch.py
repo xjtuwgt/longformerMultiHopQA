cd84630343b7a6c4d059911e86ffb07b1e46a359
guangtao.wang <guangtao.wang@jd.com>
2021-02-03 23:00:05 -0800

    longformer

--- hyperparametertuner/randomsearch.py
+++ hyperparametertuner/randomsearch.py
@@ -92,4 +92,4 @@ def generate_random_search_bash(task_num, seed=42):
     print('{} jobs have been generated'.format(task_num))
 
 if __name__ == '__main__':
-    generate_random_search_bash(task_num=1)
+    generate_random_search_bash(task_num=1, seed=1234)
