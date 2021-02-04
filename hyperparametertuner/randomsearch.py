27bcee1b9e7005cbfe282dbe83a587c6934ae4a8
guangtao.wang <guangtao.wang@jd.com>
2021-02-03 21:46:57 -0800

    longformer

---
+++ hyperparametertuner/randomsearch.py
@@ -0,0 +1,7 @@
+import os
+config_path = '../configs'
+
+learn_rate_space = [1e-5, 2e-5, 3e-5, 4e-5]
+batch_size_space = [2, 4]
+gradient_accumulation_steps_step = [2, 4]
+sent_lambda_space = [1, 5]
