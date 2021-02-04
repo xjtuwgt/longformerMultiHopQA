9bcdcd5f827016255ed896a475b48661452cedbd
guangtao.wang <guangtao.wang@jd.com>
2021-02-03 23:04:50 -0800

    longformer

--- jdattrain.py
+++ jdattrain.py
@@ -263,6 +263,9 @@ for epoch in train_iterator:
                        join(args.exp_name, f'cached_config.bin')
             )
             logger.info('Current best joint_f1 = {}'.format(best_joint_f1))
+            for key, val in metrics.items():
+                logger.info("Current {} = {}".format(key, val))
+            logger.info('*' * 100)
         torch.save({k: v.cpu() for k, v in encoder.state_dict().items()},
                     join(args.exp_name, f'encoder_{epoch+1}.pkl'))
         torch.save({k: v.cpu() for k, v in model.state_dict().items()},
