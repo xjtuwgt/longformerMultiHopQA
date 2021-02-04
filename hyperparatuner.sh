e0ba4803f7c749437d68dc520174fef2ef8b7d4b
guangtao.wang <guangtao.wang@jd.com>
2021-02-03 22:52:22 -0800

    longformer

--- hyperparatuner.sh
+++ hyperparatuner.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-#!/bin/sh
-CONFIG_PATH=configs/athgn
+JOBS_PATH=athgn_jobs
 LOGS_PATH=athgn_logs
-for ENTRY in "${CONFIG_PATH}"/*.json; do
-  echo "CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 jdattrain.py --config_file configs/athgn/${ENTRY}" > "jobs/run_${ENTRY}.sh"
-  chmod +x "jobs/run_${ENRTY}.sh"
-#  /mnt/cephfs2/asr/users/ming.tu/software/kaldi/egs/wsj/s5/utils/queue.pl -q g.q -l gpu=4 $LOGS_PATH/$FILE_NAME.log $ENTRY &
-#  sleep 20
+for ENTRY in "${JOBS_PATH}"/*.sh; do
+  chmod +x $ENTRY
+  FILE_NAME="$(basename "$ENTRY")"
+  echo $FILE_NAME
+  /mnt/cephfs2/asr/users/ming.tu/software/kaldi/egs/wsj/s5/utils/queue.pl -q g.q -l gpu=4 $LOGS_PATH/$FILE_NAME.log $ENTRY &
+  sleep 20
 done
