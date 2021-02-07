e87aed2985d6b6148af764a1afbe85dc67ec2db5
guangtao.wang <guangtao.wang@jd.com>
2021-02-07 09:49:21 -0800

    longformer

--- hyperparametertuner/randomsearch.py
+++ hyperparametertuner/randomsearch.py
@@ -46,7 +46,7 @@ def HypeParameterSpace():
     learning_rate = {'name': 'learning_rate', 'type': 'choice', 'values': [1e-5, 2e-5, 4e-5]}
     per_gpu_train_batch_size = {'name': 'per_gpu_train_batch_size', 'type': 'choice', 'values': [2]}
     gradient_accumulation_steps = {'name': 'gradient_accumulation_steps', 'type': 'choice', 'values': [8, 16]}
-    sent_lambda = {'name': 'sent_lambda', 'type': 'choice', 'values': [1, 5, 10]}
+    sent_lambda = {'name': 'sent_lambda', 'type': 'choice', 'values': [10]}
     frozen_layer_num = {'name': 'frozen_layer_number', 'type': 'choice', 'values': [0]}
     gnn_drop = {'name': 'gnn_drop', 'type': 'choice', 'values': [0.2, 0.3]}
     bi_attn_drop = {'name': 'bi_attn_drop', 'type': 'choice', 'values': [0.2, 0.3]}
