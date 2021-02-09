f6a11200a9849ae5f300745b2e2e7ff5f9c42d54
guangtao.wang <guangtao.wang@jd.com>
2021-02-09 13:08:47 -0800

    longformer

--- errorAnalysis_run.sh
+++ errorAnalysis_run.sh
@@ -36,7 +36,7 @@ preprocess() {
         [[ -d $OUTPUT_FEAT ]] || mkdir -p $OUTPUT_FEAT
 
         echo "Error Analysis"
-        python errorAnalysis/predictionAnalysis.py --raw_data $INPUT_FILE --input_dir $OUTPUT_FEAT --pred_dir $PRED_OUTPUT --output_dir $OUTPUT_FEAT --model_type roberta --model_name_or_path roberta-large --at_model_name_or_path roberta_large_test
+        python errorAnalysis/predictionAnalysis.py --raw_data $INPUT_FILE --input_dir $OUTPUT_FEAT --pred_dir $PRED_OUTPUT --output_dir $OUTPUT_FEAT --model_type roberta --model_name_or_path roberta-large --at_model_name_or_path train.roberta.bs2lr1e-05.seed42
 #        python errorAnalysis/predictionAnalysis.py --raw_data $INPUT_FILE --input_dir $OUTPUT_FEAT --pred_dir $PRED_OUTPUT --output_dir $OUTPUT_FEAT --model_type roberta --model_name_or_path roberta_large_test
 
 #        python errorAnalysis/predictionAnalysis.py --raw_data $INPUT_FILE --input_dir $OUTPUT_FEAT --pred_dir $PRED_OUTPUT --output_dir $OUTPUT_FEAT --model_type albert --model_name_or_path albert-xxlarge-v2
