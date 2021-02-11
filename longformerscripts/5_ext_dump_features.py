a83a1373e93620efc5ce5434cf453c785b762fbd
guangtao.wang <guangtao.wang@jd.com>
2021-02-10 18:38:25 -0800

    longformer

--- longformerscripts/5_ext_dump_features.py
+++ longformerscripts/5_ext_dump_features.py
@@ -394,6 +394,8 @@ def convert_examples_to_features(examples, tokenizer, max_seq_length, max_query_
         all_doc_tokens += all_query_tokens
 
         for (i, token) in enumerate(example.doc_tokens):
+            if token == ' ':
+                print('here')
             orig_to_tok_index.append(len(all_doc_tokens))
             if is_roberta:
                 sub_tokens = tokenizer.tokenize(token, add_prefix_space=True)
