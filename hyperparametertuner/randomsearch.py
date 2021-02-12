f3b39a89b7022b42018717791a9827c7ea4a3759
guangtao.wang <guangtao.wang@jd.com>
2021-02-12 10:10:00 -0800

    longformer

--- hyperparametertuner/randomsearch.py
+++ hyperparametertuner/randomsearch.py
@@ -43,10 +43,10 @@ def rand_search_parameter(space: dict):
         raise ValueError('Training batch mode %s not supported' % para_type)
 
 def HypeParameterSpace():
-    learning_rate = {'name': 'learning_rate', 'type': 'choice', 'values': [2e-5, 4e-5]}
-    per_gpu_train_batch_size = {'name': 'per_gpu_train_batch_size', 'type': 'choice', 'values': [2]}
-    gradient_accumulation_steps = {'name': 'gradient_accumulation_steps', 'type': 'choice', 'values': [8, 16]}
-    sent_lambda = {'name': 'sent_lambda', 'type': 'choice', 'values': [10, 15]}
+    learning_rate = {'name': 'learning_rate', 'type': 'choice', 'values': [1e-5, 2e-5, 4e-5]}
+    per_gpu_train_batch_size = {'name': 'per_gpu_train_batch_size', 'type': 'choice', 'values': [2, 4]}
+    gradient_accumulation_steps = {'name': 'gradient_accumulation_steps', 'type': 'choice', 'values': [4, 8]}
+    sent_lambda = {'name': 'sent_lambda', 'type': 'choice', 'values': [15, 20]}
     frozen_layer_num = {'name': 'frozen_layer_number', 'type': 'choice', 'values': [0]}
     gnn_drop = {'name': 'gnn_drop', 'type': 'choice', 'values': [0.2, 0.3]}
     bi_attn_drop = {'name': 'bi_attn_drop', 'type': 'choice', 'values': [0.2, 0.3]}
