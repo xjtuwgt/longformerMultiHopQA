ade8f480e44d246853f8bbe705462a382305f0dc
guangtao.wang <guangtao.wang@jd.com>
2021-02-16 22:32:13 -0800

    longformer

--- unifiedqa_long_run.sh
+++ unifiedqa_long_run.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 # DEFINE data related (please make changes according to your configurations)
-# DATA ROOT folder where you put data files
+# DATA ROOT folder where you put data files, transformers 3.3.0
 DATA_ROOT=./data/
+LONG_FORMER_ROOT=allenai
+SELECTEED_DOC_NUM=4
 
 
 PROCS=${1:-"download"} # define the processes you want to run, e.g. "download,preprocess,train" or "preprocess" only
 
 # define precached BERT MODEL path
-ROBERTA_LARGE=$DATA_ROOT/models/pretrained/roberta-large
+# ROBERTA_LARGE=$DATA_ROOT/models/pretrained/roberta-large
+# pip install -U spacy
+# python -m spacy download en_core_web_lg
 
 # Add current pwd to PYTHONPATH
 export DIR_TMP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -59,20 +63,27 @@ preprocess() {
 #        # Output: ner.json
 #        python scripts/2_extract_ner.py $INPUT_FILE $OUTPUT_PROCESSED/doc_link_ner.json $OUTPUT_PROCESSED/ner.json
 #
-#        echo "3. Paragraph ranking"
+#        echo "3. Paragraph ranking (1)"
 #        # Output: para_ranking.json
 #        python scripts/3_prepare_para_sel.py $INPUT_FILE $OUTPUT_PROCESSED/hotpot_ss_$DATA_TYPE.csv
 #
-#        # switch to RoBERTa for final leaderboard
-#        python scripts/3_paragraph_ranking.py --data_dir $OUTPUT_PROCESSED --eval_ckpt $DATA_ROOT/models/finetuned/PS/pytorch_model.bin --raw_data $INPUT_FILE --input_data $OUTPUT_PROCESSED/hotpot_ss_$DATA_TYPE.csv --model_name_or_path roberta-large --model_type roberta --max_seq_length 256 --per_gpu_eval_batch_size 128 --fp16
+#        echo "3. Paragraph ranking (2): longformer retrieval data preprocess"
+#        # Output: para_ranking.json
+#        python longformerscripts/3_longformer_prepare_para_sel.py $INPUT_FILE $OUTPUT_PROCESSED/para_ir_combined.json
+#
+#        echo "3. Paragraph ranking (3): longformer retrieval ranking scores"
+#
+#        # switch to Longformer for final leaderboard
+#        python longformerscripts/3_longformer_paragraph_ranking.py --data_dir $OUTPUT_PROCESSED --eval_ckpt $DATA_ROOT/models/finetuned/PS/longformer_pytorchlighting_model.ckpt --raw_data $INPUT_FILE --input_data $OUTPUT_PROCESSED/para_ir_combined.json
+#
+        echo "4. MultiHop Paragraph Selection (4)"
+        # Input: $INPUT_FILE, doc_link_ner.json,  ner.json, long_para_ranking.json
+        # Output: long_multihop_para.json
+        python longformerscripts/4_longformer_multihop_ps.py $INPUT_FILE $OUTPUT_PROCESSED/doc_link_ner.json $OUTPUT_PROCESSED/ner.json $OUTPUT_PROCESSED/long_para_ranking.json $OUTPUT_PROCESSED/long_multihop_para.json $SELECTEED_DOC_NUM
 
-        echo "4. MultiHop Paragraph Selection"
-        # Input: $INPUT_FILE, doc_link_ner.json,  ner.json, para_ranking.json
-        # Output: multihop_para.json
-        python scripts/4_multihop_ps.py $INPUT_FILE $OUTPUT_PROCESSED/doc_link_ner.json $OUTPUT_PROCESSED/ner.json $OUTPUT_PROCESSED/para_ranking.json $OUTPUT_PROCESSED/multihop_para.json
+        echo "5. Dump features (5)"
+        python longformerscripts/5_ext_unifiedqa_dump_features.py --para_path $OUTPUT_PROCESSED/long_multihop_para.json --full_data $INPUT_FILE --model_name_or_path $LONG_FORMER_ROOT/unifiedqa-t5-large --ner_path $OUTPUT_PROCESSED/ner.json --model_type unifiedqa --tokenizer_name $LONG_FORMER_ROOT/unifiedqa-t5-large --output_dir $OUTPUT_FEAT --doc_link_ner $OUTPUT_PROCESSED/doc_link_ner.json --max_para_num $SELECTEED_DOC_NUM
 
-        echo "5. Dump features"
-        python longformerscripts/5_ext_unifiedqa_dump_features.py --para_path $OUTPUT_PROCESSED/multihop_para.json --full_data $INPUT_FILE --model_name_or_path $LONG_FORMER_ROOT/unifiedqa-t5-large --ner_path $OUTPUT_PROCESSED/ner.json --model_type unifiedqa --tokenizer_name $LONG_FORMER_ROOT/unifiedqa-t5-large --output_dir $OUTPUT_FEAT --doc_link_ner $OUTPUT_PROCESSED/doc_link_ner.json --max_para_num $SELECTEED_DOC_NUM
     done
 
 }
