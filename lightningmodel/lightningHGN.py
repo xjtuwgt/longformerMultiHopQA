e1805b67c00bde9881719225eb781f0fffa1042a
guangtao.wang <guangtao.wang@jd.com>
2021-02-18 11:08:04 -0800

    longformer

--- lightningmodel/lightningHGN.py
+++ lightningmodel/lightningHGN.py
@@ -41,11 +41,11 @@ class lightningHGN(pl.LightningModule):
             self.model.load_state_dict(torch.load(model_path))
 
     def prepare_data(self):
-        self.helper = DataHelper(gz=True, config=self.args)
-        self.train_data = self.helper.train_loader
-        self.dev_example_dict = self.helper.dev_example_dict
-        self.dev_feature_dict = self.helper.dev_feature_dict
-        self.dev_data = self.helper.dev_loader
+        helper = DataHelper(gz=True, config=self.args)
+        self.train_data = helper.train_loader
+        self.dev_example_dict = helper.dev_example_dict
+        self.dev_feature_dict = helper.dev_feature_dict
+        self.dev_data = helper.dev_loader
 
     def setup(self, stage: str):
         if stage == 'fit':
