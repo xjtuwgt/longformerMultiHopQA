cb4f6656bd036b5aee86daab0e35aa9e5f789f8c
guangtao.wang <guangtao.wang@jd.com>
2021-02-18 11:49:25 -0800

    longformer

--- lightningtrain.py
+++ lightningtrain.py
@@ -39,8 +39,7 @@ def main(args):
     pl.seed_everything(args.seed)
     #########################################################################
     model = lightningHGN(args=args)
-    model.prepare_data()
-
+    # model.prepare_data()
     # model.setup()
     return
 
